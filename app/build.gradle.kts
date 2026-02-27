93a7a610f1ed88a7d36586576df5c306c7739c53
Joaquín Megías <megiasmultimedia@gmail.com>
2026-02-27 19:16:08 -0300

    Hilt configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.hilt.navigation)
     implementation(libs.hilt.android)
     implementation(libs.retrofit)
     implementation(libs.retrofit.serialization)
--- app/src/main/java/com/megias/weatherapp/MainActivity.kt
+++ app/src/main/java/com/megias/weatherapp/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.megias.weatherapp.ui.theme.WeatherAppTheme
+import com.megias.weatherapp.ui.weather.WeatherScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +22,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            MaterialTheme {
+                WeatherScreen()
+            }
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
